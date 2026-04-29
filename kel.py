96afcfc6a00312a1874df3636292b898e4822471
xabibulloshamsiddinov <xabibulloshamsiddinov90@gmail.com>
2026-04-29 17:08:01 +0500

    Update kel.py

--- kel.py
+++ kel.py
@@ -1,1 +1,13 @@
-print("here is hello world")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "here is hello world"
+
+if __name__ == "__main__":
+    # Render o'zi port tayinlaydi, bo'lmasa 10000 ishlatiladi
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
